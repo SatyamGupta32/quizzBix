e1cc7e9cfc36e35f4e88f003b9265aa93320352d
Satyam Gupta <satyamg501@gmail.com>
2024-11-30 16:08:17 +0530

    first commit

---
+++ src/components/data/topics.js
@@ -0,0 +1,58 @@
+const topics = [
+    {
+      category: "General Online Tests",
+      items: [
+        "Online Aptitude Test",
+        "Online Verbal Ability Test",
+        "Online Logical Reasoning Test",
+        "Online Verbal Reasoning Test",
+        "Online Non Verbal Reasoning Test",
+        "Online Data Interpretation Test",
+        "Online General Knowledge Test",
+        "Online Current Affairs Test",
+      ],
+    },
+    {
+      category: "Programming Online Tests",
+      items: [
+        "Online C Programming Test",
+        "Online C# Programming Test",
+        "Online C++ Programming Test",
+        "Online Java Programming Test",
+      ],
+    },
+    {
+      category: "Technical Online Tests",
+      items: [
+        "Online Database Test",
+        "Online Networking Test",
+        "Online Computer Science Test",
+      ],
+    },
+    {
+      category: "Engineering Online Tests",
+      items: [
+        "Online Civil Engineering Test",
+        "Online Mechanical Engineering Test",
+        "Online Electrical Engineering Test",
+        "Online Electronics and Communication Engineering Test",
+        "Online Electronics Test",
+        "Online Digital Electronics Test",
+        "Online Electronic Devices Test",
+        "Online Technical Drawing Test",
+        "Online Engineering Mechanics Test",
+        "Online Chemical Engineering Test",
+      ],
+    },
+    {
+      category: "Medical Science Online Tests",
+      items: [
+        "Online Microbiology Test",
+        "Online Biotechnology Test",
+        "Online Biochemistry Test",
+        "Online Biochemical Engineering Test",
+      ],
+    },
+  ];
+
+  export default topics;
