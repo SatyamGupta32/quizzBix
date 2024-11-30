93d09ba0f14a6259c6dd0f1ddae84ad9b592d7d8
Satyam Gupta <satyamg501@gmail.com>
2024-11-30 19:22:51 +0530

    first commit

--- src/components/AllTopics.jsx
+++ src/components/AllTopics.jsx
@@ -24,14 +24,14 @@ const AllTopics = () => {
         {topics.map((topic, index) => (
           <TopicItem key={index}>
             <FlexImg>
-              <Icon src="/icons/list-icon.svg" alt="List Icon" />
+              <Icon src="/list-icon.svg" alt="List Icon" />
               <TopicCategory>{topic.category}</TopicCategory>
             </FlexImg>
             <CategoryLine />
             <TopicsList>
               {topic.items.map((item, idx) => (
                 <TestItem key={idx}>
-                  <Icon src="/icons/test-icon.svg" alt="Test Icon" />
+                  <Icon src="/test-icon.svg" alt="Test Icon" />
                   <StyledLink to={`/topics/${encodeURIComponent(item)}`}>
                     {item}
                   </StyledLink>
